660f6179e9e30ff8d607fdc303bf55d6d4ab14cc
Eduardo Bernardi Amaral <55240037+amaraleb@users.noreply.github.com>
2022-07-19 14:36:32 -0300

    Add files via upload

--- app/app.js
+++ app/app.js
@@ -2,9 +2,9 @@ console.log("olá mundo!");
 
 const log = ["mesmo", "teste", "outro", "edu"];
 const key = ["mesmo", "teste", "outro", "1807"];
-const cn = ["mesmo", "teste", "outro", "Iraí"];
+const cn = ["mesmo", "teste", "outro", "Eduardo Bernardi Amaral"];
 const bd = ["mesmo", "teste", "outro", "18071990"];
-const ct = ["mesmo", "teste", "outro", "1807"];
+const ct = ["mesmo", "teste", "outro", "Iraí"];
 
 
 var user;
@@ -12,6 +12,7 @@ var psw;
 var cname;
 var bdate;
 var city;
+var cpsw
 
 
 function Entrar() {
@@ -24,7 +25,7 @@ function Entrar() {
     if(i>=0){
         if(psw==key[i]){
             window.location="profile.html";
-            element.innerHTML = '<b>Hello World!</b>'
+            localStorage.setItem('x', i)
         } else{
             window.alert("Senha inválida");
         }
@@ -42,16 +43,41 @@ function Enviar() {
     if(i>=0){
         window.alert("Usuário já cadastrado");
     } else {
-    cname = $("#cname").val();
-    bdate = $("#bdate").val();
-    city = $("#city").val();
-    psw = $("#psw").val();
-    log.push(user);
-    cn.push(cname);
-    bd.push(bdate);
-    ct.push(city);
-    key.push(psw);
-    window.alert("Usuário cadastrado com sucesso!");
-    window.location.href="index.html";
+        cname = $("#cname").val();
+        bdate = $("#bdate").val();
+        city = $("#city").val();
+        psw = $("#psw").val();
+        cpsw = $('#cpsw').val();
+        if(psw == cpsw) {
+            log.push(user);
+            cn.push(cname);
+            bd.push(bdate);
+            ct.push(city);
+            key.push(psw);
+            window.alert("Usuário cadastrado com sucesso!");
+            window.location.href="index.html";
+        } else {
+            window.alert("Confirmação da senha diferente da senha");
+        }
     }  
 }
+
+function Perfil() {
+    if(localStorage.length>0) {
+        let f = localStorage.getItem('x');
+        $("#cname").append(cn[f]);
+        $("#bdate").append(bd[f]);
+        $("#city").append(ct[f]);
+    } else {
+        window.location.href="index.html";
+    }
+}
+
+function Clean() {
+    localStorage.clear();
+    window.location.href="index.html";
+}
+
+function Refresh(){
+    window.location.reload();
+  }
